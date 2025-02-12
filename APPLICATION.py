63429d3373a40b3026cc35fb3cad131d3802fd44
Aya2025-cmyk <ayambathieu8@gmail.com>
2025-02-12 23:08:07 +0000

    Add files via upload

--- APPLICATION.py
+++ APPLICATION.py
@@ -54,7 +54,7 @@ def add_bg_from_local(image_file):
 
 
 # Fond d'écran de l'application
-add_bg_from_local("samba.webp")
+add_bg_from_local("logo.webp")
 
 # Fonction pour convertir un DataFrame en CSV
 @st.cache_data
